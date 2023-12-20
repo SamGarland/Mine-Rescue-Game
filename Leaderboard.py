66b37596b00a4f8746eac910f564172cf338e6eb
Sam Garland <samuel.p.garland@gmail.com>
2023-12-20 20:51:48 +0000

    Changes to Leaderboard.py
    Unfinished alterations to the functions in leaderboard.py

--- Leaderboard.py
+++ Leaderboard.py
@@ -23,11 +23,11 @@ class Leaderboard():
         return (f"{self._username},{self._status},{self._puzzle_time},{self._agg_time}")
 
 
-    
-
     def update_leaderboard(username, puzzle, count_time):
-        
+        # This isn't working as expected: the issue is that it can't find lines that are not new to the file, but don't have the username being looked for to tranfer
+        #them to the new file...
         usernames = []
+        line_set = []
         
         with open("leaderboard.txt", "r+") as lb, open("new_leaderboard.txt", "a+") as new_lb:
             
@@ -36,28 +36,32 @@ class Leaderboard():
             for line in lines:
                                 
                 line = line.strip("\n").split(",")
-                str_line = f"{line[0]},{line[1]},{line[2]},{line[3]}"
                 
                 usernames.append(line[0])
+                line_set.append(line)
+                print(usernames)
+                print(line_set)
                 
-                if line[0] != username:
+                if line[0] == username:
                     
-                    new_lb.write(str_line)
-                
-                elif line[0] == username:
-                    
-                    l_3 = int(line[3])
-                    c_t = int(count_time)
-                    
-                    agg_time = l_3 + c_t
+                    agg_time = float(line[3]) + float(count_time)
                     
                     update = Leaderboard(username, puzzle, count_time, agg_time)
                     
-                    new_lb.write(str(update))
+                    new_lb.write(f"{str(update)}\n")
                     
                 else:
                     continue
-        lb.close()
+        
+            for line in line_set:
+                
+                if line[0] != username:
+                
+                    new_lb.write(f"{line[0]},{line[1]},{line[2]},{line[3]}\n")
+                
+                else:
+                    continue
+            
         os.remove("Leaderboard.txt")
         os.rename("new_leaderboard.txt", "Leaderboard.txt")
         
@@ -69,26 +73,59 @@ class Leaderboard():
             try:    
                 if len(usernames) == 0:
                     
-                    lb.write(str(new_entry))
+                    lb.write(f"{str(new_entry)}\n")
                 
                 else:
                     try:
                         if username not in usernames: 
-                            lb.write(str(new_entry))
+                            lb.write(f"{str(new_entry)}\n")
                     except:
                         print("We got an issue updating the leaderboard!")
             except:
                 pass
         
-        lb.close
+        lb.close()
         
-            
-'''
-    
-show_leaderboard():
-    this prints current leaderboard to console in a table
-    
-count_time(start_time, end_time):
-    this takes the start_time from the end_time to give the time it took to complete a puzzle.
+    def show_Leaderboard():
 
-'''
+        # This function takes each line from the file and displays a leaderboard.
+        
+        def underline(text):
+            print("\u0332".join(text))
+        
+        with open("Leaderboard.txt", "r") as lb:
+            
+            lines = lb.readlines()
+            
+            file_set = []
+            agg_times_set = []
+            
+            for line in lines:
+                line = line.strip("\n").split(",")
+                file_set.append(line)
+                agg_times_set.append(line[3])
+            
+            agg_times_set.sort()
+            
+            text = f"\n\nPosition    Username   Status    Current_Puzzle_Time  Total_Puzzle_Time\n"
+            underline(text)
+            
+            count = 1
+            for item in file_set:
+                for ele in agg_times_set:
+                    if item[3] == ele:
+                        print(f" #{count}          {item[0]}    {item[1]}  {item[2]}  {item[3]}\n")
+                        count += 1
+        lb.close()
+            
+    def count_time(start_time, end_time):
+        
+        puzzle_time = end_time - start_time
+        
+        return puzzle_time
+
+
+
+Leaderboard.update_leaderboard("John", "puzzle_two", time.time())
+
+#Leaderboard.show_Leaderboard()
